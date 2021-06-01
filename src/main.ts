991f296a4789293e3b64f82c11ff251423c8e4e1
Tim Smart <tim.smart@arisechurch.com>
2021-06-01 13:56:19 +1200

    Add top.gg update

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,36 @@ require("dotenv").config();
 
 import { createClient, Intents } from "droff";
 import { MongoClient } from "mongodb";
+import * as RxO from "rxjs/operators";
+import Topgg from "@top-gg/sdk";
 
 async function main() {
   const client = createClient({
     token: process.env.DISCORD_BOT_TOKEN!,
     intents: Intents.GUILDS,
   });
-
   const mongo = await MongoClient.connect(process.env.MONGODB_URI!, {
     useUnifiedTopology: true,
   });
   const db = mongo.db(process.env.MONGODB_DB!);
+  const topgg = new Topgg.Api(process.env.TOPGG_TOKEN!);
 
-  const commands = client.useSlashCommands();
+  // Send stats to top.gg
+  client.guilds$.pipe(
+    RxO.auditTime(60000),
+    RxO.flatMap((guilds) => {
+      const serverCount = guilds.count();
+      console.log("[main.ts]", "Updating top.gg serverCount", serverCount);
+      return topgg.postStats({
+        serverCount,
+      });
+    }),
+  );
 
   // Register commands
+  const commands = client.useSlashCommands();
+  // poll
   require("./polls/command").register(commands, client, db);
-
   commands.start();
 }
 
